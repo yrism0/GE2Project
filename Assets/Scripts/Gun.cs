c7a8bc43b09c6c21ac3e3ead4272a32f519d0049
cold <blairmarc963@gmail.com>
2025-11-21 15:10:50 +0000

    working ammo system

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,24 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
     public float gunDamage = 10f;
     public float range = 100f;
+    public int ammo = 0;
+    public int revammo = 0;
+    public int ammodiff;
 
+    public Text Acount;
     public Camera fpsCam;
     public LayerMask mask;
 
+    public void Start()
+    {
+        ammo = 7;
+        revammo = 21;
+        ammodiff = 0;
+    }
+
     private void Update()
     {
+        Acount.text = ammo.ToString() + "/" + revammo.ToString();
         Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward, Color.green);
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") & ammo > 0 )
         {
             Shoot();
+        }
+
+        if (Input.GetKeyDown("r") & ammo < 7 & revammo > 0)
+        {
+            ammo += ammodiff;
+            revammo -= ammodiff;
+            ammodiff = 0;
+        }
+        if (Input.GetKeyDown("k"))
+        {
+            revammo = 100;
         }
     }
 
     void Shoot()
     {
+        ammo -= 1;
+        ammodiff += 1;
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, ~mask))
         {

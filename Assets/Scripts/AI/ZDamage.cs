365b38118ffa7120ff73f16ec56a58b553c71929
Sharkwaves <b01814233@studentmail.uws.ac.uk>
2025-11-22 20:36:55 +0000

    fixed zombie damage
    fixed it

--- Assets/Scripts/AI/ZDamage.cs
+++ Assets/Scripts/AI/ZDamage.cs
@@ -6,6 +6,10 @@ public class ZDamage : MonoBehaviour
     public PlayerHealth playerhealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    private void Start()
+    {
+        playerhealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
